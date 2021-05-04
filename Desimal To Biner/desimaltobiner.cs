e480d8832eb2012f048cd03f2c63bbbb6eb334b6
hadisubakti <hadisubakti0205@gmai.com>
2021-05-04 23:30:01 +0700

    UTS PBO

---
+++ Desimal To Biner/desimaltobiner.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Desimal_To_Biner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Tugas Program Konversi Dari Bilangan Desimal Ke Biner C#");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Nama        : Hadi Subakti");
+            Console.WriteLine("NPM         : 1101201002");
+            Console.WriteLine("Prodi       : Teknik Informatika (Malam)");
+            Console.WriteLine("Semester    : 2 (Genap)");
+            Console.WriteLine("Mata Kuliah : Pemrograman Berorientasi Objek");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.Write("Input Angka Desimal Yang akan di Konversi : ");
+
+
+            int bilangan, sisa;
+            string hasil = "";
+            bilangan = Convert.ToInt32(Console.ReadLine());
+            while (bilangan > 0)
+            {
+                sisa = bilangan % 2;
+                bilangan /= 2;
+                hasil = sisa.ToString() + hasil;
+            }
+            Console.WriteLine("Hasil Konversi Ke Biner : {0}", hasil);
+        }
+    }
+}
